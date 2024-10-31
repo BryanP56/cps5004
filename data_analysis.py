d558e6bb3bb85a4c232b1bef95acec8e530f3dcc
BryanP56 <2313697@live.stmarys.ac.uk>
2024-10-31 17:26:33 +0000

    libraries

---
+++ data_analysis.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_curve
+from sklearn.cluster import KMeans
+from sklearn.linear_model import LogisticRegression
+from scipy.stats import ttest_ind
